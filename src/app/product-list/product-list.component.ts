b56b9cc5928176cde5f89c549219c4f4e358656a
yotech-21 <yotech21@uiigo.sylhet>
2024-02-27 19:29:22 +0600

    middle of the implementation of service for api

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  constructor() {}
+
+  http = inject(HttpClient);
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.http.get('https://dummyjson.com/products')
+    .subscribe((data: any) => {
+      console.log(data.products);
+      this.products = data.products;
+    })
+  }
 }
